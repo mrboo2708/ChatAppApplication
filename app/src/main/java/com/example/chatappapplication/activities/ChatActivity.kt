c3bb11397374304ff654eab505dee2774c79ce61
Thanh Luan Nguyen <mrboo2707@gmail.com>
2023-02-26 21:52:53 +0700

    finish UI for chat message

---
+++ app/src/main/java/com/example/chatappapplication/activities/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chatappapplication.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatappapplication.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
